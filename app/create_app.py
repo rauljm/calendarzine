4c813f26b92dadcbcf8546fa8bfb001503d67d6c
rauljm <raulmartinsj@outlook.com>
2018-10-10 20:45:49 -0300

    creating all models to application

--- app/create_app.py
+++ app/create_app.py
@@ -5,16 +5,19 @@ from flask_migrate import Migrate
 from models import db
 
 
-def create_app():
+def create_app(testing=False):
     app = Flask(__name__)
     # api = Api(app)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/app.db'
+    if testing:
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/app.db'
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    db.init_app(app)
 
     from models.user import User
     from models.schedule import Schedule
+    from models.room import Room
+    db.init_app(app)
     Migrate(app, db)
 
     return app
--- app/models/schedule.py
+++ app/models/schedule.py
@@ -6,5 +6,9 @@ class Schedule(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.Date, nullable=False)
+
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     user = db.relationship('User', backref=db.backref('schedules', lazy=True))
+
+    room_id = db.Column(db.Integer, db.ForeignKey('room.id'))  # this is not nullable because of a bug in sqlite :(
+    room = db.relationship('Room', backref=db.backref('schedules', lazy=True))
